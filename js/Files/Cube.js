aa6d3aca20e324a24908bfe017230595cf2e6c54
Brian Peiris <brianpeiris@gmail.com>
2015-11-28 23:43:41 -0500

    Remove defaultContents

--- js/Files/Cube.js
+++ js/Files/Cube.js
@@ -1,10 +1,24 @@
 var t3 = THREE;
+
 var light = new t3.PointLight();
-light.position.set(0, 10, 0);
+light.position.set(1, 1, 1);
 scene.add(light);
-scene.add(new t3.Mesh (
-  new t3.BoxGeometry(1, 1, 1),
-  new t3.MeshLambertMaterial({
-    color: 'red'
-  })
-));
+
+function makeCube (x, y, z) {
+  var cube = new t3.Mesh(
+    new t3.BoxGeometry(1, 1, 1),
+    new t3.MeshLambertMaterial({
+      color: 'red'
+    })
+  );
+  cube.position.set(x, y, z);
+  return cube;
+}
+var cube = makeCube(0, 0, 0);
+scene.add(cube);
+
+var i = 0;
+return function () {
+  cube.rotation.y = i;
+  i += 0.02;
+};
